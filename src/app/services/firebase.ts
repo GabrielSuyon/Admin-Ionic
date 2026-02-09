992a117e7eaae07c081f6e3e6f93c8b378094d32
GabrielSuyon <vitealonso59@gmail.com>
2026-02-08 20:07:34 -0500

    Actualización completa del proyecto

--- src/app/services/firebase.ts
+++ src/app/services/firebase.ts
@@ -3,7 +3,7 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
 import {User} from '../models/user.model'
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { getFirestore, setDoc, doc } from '@angular/fire/firestore';
+import { getFirestore, setDoc, doc, getDoc } from '@angular/fire/firestore';
 @Injectable({
   providedIn: 'root'
 })
@@ -40,6 +40,10 @@ export class FirebaseService {
     //== Setear un documento ====
     setDocument(path: string, data: any){
       return setDoc(doc(getFirestore(),path),data);
+    }
+    //== Setear un documento ====//
+    async getDocument(path: string){
+      return(await getDoc(doc(getFirestore(),path))).data();
     }
 
 }
